978ba8f486f84fa8e300282b441231fbc60b8947
paulocesarloayza@upsa.edu.bo <paulocesarloayza@upsa.edu.bo>
2019-08-31 13:20:36 -0400

    Web api

---
+++ mvcPhone/apiPhone/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace apiPhone.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
