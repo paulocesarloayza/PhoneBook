27db78516258038a4ef9a63286c4eb3c8e5aae79
paulocesarloayza@upsa.edu.bo <paulocesarloayza@upsa.edu.bo>
2019-10-12 13:16:36 -0400

    MVVM completo

---
+++ mvcPhone/appPhone/appPhone/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace appPhone.ViewModels
+{
+    using appPhone.Models;
+    using System.Collections.Generic;
+
+    public class MainViewModel
+    {
+        #region Properties
+        public List<Phone> ListPhone { get; set; }
+        #endregion
+
+        #region ViewModels
+        public PhoneBookViewModel phoneBookViewModel { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            instance = this;
+            phoneBookViewModel=new PhoneBookViewModel();
+        } 
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if(instance==null)
+            {
+                instance = new MainViewModel();
+            }
+            return (instance);
+        }
+        #endregion
+    }
+}
